5846278ee3f8ddc869bdedc8947a43506609569c
Sean Dawson <spdawson@agiledigital.com.au>
2020-08-11 19:13:59 +1000

    build: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "browser": true,
+        "jest": true,
+        "es2020": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 11
+    }
+};

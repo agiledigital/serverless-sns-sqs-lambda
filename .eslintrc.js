b415601b6cd86f52efd557741e8d131be4f65d2a
Sean Dawson <contact@seandawson.info>
2020-11-02 18:49:04 +1100

    chore: clean up code and improve test coverage
    - Add a prettier config for formatting consistency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "browser": true,
-        "jest": true,
-        "es2020": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 11
-    }
+  env: {
+    commonjs: true,
+    browser: true,
+    jest: true,
+    es2020: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 11
+  }
 };
